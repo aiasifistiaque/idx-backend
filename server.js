96b995d409437a1ef4595095dd3b806c21b86d91
Asif Istiaque <asifistiaque.ai@gmail.com>
2022-05-13 04:06:48 +0600

    asds

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const httpServer = createServer(app);
 export const io = new Server(httpServer, {
 	serveClient: false,
 	cors: {
-		origin: 'http://localhost:3000',
+		origin: 'https://idx-client.vercel.app',
 	},
 });
 
